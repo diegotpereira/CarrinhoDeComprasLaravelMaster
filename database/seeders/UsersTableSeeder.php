0d3d666f091e10e22bc68cda1fd13b9c9e60bef1
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-06-17 17:36:04 -0300

    adicionado componente Autenticador de usuário.

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Usuario',
+            'email' => 'usuario@usuario.com',
+            'password' => bcrypt('password'),
+        ]);
+    }
+}
