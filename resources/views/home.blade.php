652ed602edc315b66f63bb8a39636267fd2ec37c
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-06-17 12:18:21 -0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Painel de Controle</div>
+
+                <div class="card-body">
+
+                       <div class="alert alert-success" role="alert">
+
+                       </div>
+
+
+                    Loja Virtual
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr>
+
+    <a href="">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header"></div>
+
+                    <div class="card-body">
+
+                           <div class="alert alert-success" role="alert">
+
+                           </div>
+
+                        <h5></h5>
+                        <br>
+                        <h5></h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </a>
+    <hr>
+
+</div>
+
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=">
+
+    <!-- CSRF Token -->
+
+
+    <title>Carrinho de Compras</title>
+
+    <!-- Scripts -->
+
+    <script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+
+</head>
+
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{url('/')}}">Carrinho de Compras</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-targer="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{__('Toggle navigation')}}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- NavBar lado esquerdo -->
+                    <ul class="navbar-nav mr-auto">
+                    </ul>
+                    <!-- Navbar lado direito-->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Links de Autenticação -->
+                        @guest
+                        <li class="nav-item">
+                            <a class="navbar-link" href="#">
+
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+
+                            </a>
+                        </li>
+
+                        @else
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Minhas Compras</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Carrinho de Compras</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="" onclick="">
+                                </a>
+
+                                <form id="logout-form" action="" method="POST" style="display: none;">
+                                   @csrf
+                                </form>
+                            </div>
+                        </li>
+                     @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+        @stack('scripts')
+    </div>
+</body>
+</html>
