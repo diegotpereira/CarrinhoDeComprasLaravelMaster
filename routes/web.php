71b2e71215e6a894c02e037d8b81446e23b61fd9
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-06-17 17:36:59 -0300

    alteração no arquivo de rotas.

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,18 @@ use Illuminate\Support\Facades\Route;
     return view('welcome');
 });*/
 
+Auth::routes();
+
+// Rotas do carrinho de compras
+Route::get('/carrinho', 'CarrinhoController@index')->name('carrinho.index');
+
+Route::post('/carrinho/adicionar', 'CarrinhoController@adicionar')->name('carrinho.adicionar');
 
 //Rotas da loja
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/{id}/show', 'HomeController@show')->name('home.show');
+
+
+
+/*Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home'); */
